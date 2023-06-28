0ad4c429657158e91ec5ba28a302b846cccae880
cesar <cesar.romero@gmail.com>
2023-06-28 00:21:17 -0700

    fix figure size bug

--- hexbin.py
+++ hexbin.py
@@ -9,7 +9,7 @@ y = 2*np.random.standard_normal(n)
 
 p = figure(title="Hexbin for 500 points", match_aspect=True,
            tools="wheel_zoom,reset", background_fill_color='#111111',
-           plot_width=1024, plot_height=1024)
+           width=1024, height=1024)
 p.grid.visible = False
 
 r, bins = p.hexbin(x, y, size=0.5, hover_color="pink", hover_alpha=0.8)
