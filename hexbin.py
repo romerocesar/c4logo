820517797fde20a0f21ea883e72cca7146a1a8e1
cesar <cesar.romero@gmail.com>
2023-06-28 00:14:52 -0700

    add requirements and initial hexbin logo code

---
+++ hexbin.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from bokeh.models import HoverTool
+from bokeh.plotting import figure, show
+
+n = 500
+x = 2*np.random.standard_normal(n)
+y = 2*np.random.standard_normal(n)
+
+p = figure(title="Hexbin for 500 points", match_aspect=True,
+           tools="wheel_zoom,reset", background_fill_color='#111111',
+           plot_width=1024, plot_height=1024)
+p.grid.visible = False
+
+r, bins = p.hexbin(x, y, size=0.5, hover_color="pink", hover_alpha=0.8)
+
+p.circle(x, y, color="white", size=1)
+
+p.add_tools(HoverTool(
+    tooltips=[("count", "@c"), ("(q,r)", "(@q, @r)")],
+    mode="mouse", point_policy="follow_mouse", renderers=[r]
+))
+
+show(p)
